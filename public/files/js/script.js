eae13da53c9e0a34f9c8c3b032906b01dd561a13
okawa-h <ookawa124@gmail.com>
2018-03-23 19:24:13 +0900

    fix get user camera

--- public/files/js/script.js
+++ public/files/js/script.js
@@ -138,7 +138,7 @@ Main.loadImage = function(src) {
 };
 Main.loadVideo = function(src) {
 	Main.log("Analyze...");
-	var constraints = { audio : false, video : { facingMode : { exact : "environment"}}};
+	var constraints = { audio : false, video : { facingMode : "user"}};
 	var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 	var tmp = window.navigator;
 	getUserMedia.call(tmp,constraints,function(stream) {
--- src/Main.hx
+++ src/Main.hx
@@ -236,7 +236,7 @@ class Main {
 		// _video.src = src;
 
 		// var constraints:MediaStreamConstraints = { video:true,audio:false };
-		var constraints:MediaStreamConstraints = untyped { audio:false,video:{ facingMode: { exact: 'environment' } }};
+		var constraints:MediaStreamConstraints = untyped { audio:false,video:{ facingMode:'user' }};
 		var getUserMedia = untyped __js__('navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia');
 		getUserMedia.call(Browser.navigator,constraints,function (stream:MediaStream) {
 
